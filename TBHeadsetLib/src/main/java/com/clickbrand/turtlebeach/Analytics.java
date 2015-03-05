1c0d6b79e126ef7d93f3ff899155c36b7e1aa811
Elizabeth Mezias <bethmezias@aim.com>
2015-03-05 16:47:41 -0500

    removing imported project, will create with copy/paste

--- TBHeadsetLib/src/main/java/com/clickbrand/turtlebeach/Analytics.java
+++
@@ -1,7 +0,0 @@
-package com.clickbrand.turtlebeach;
-
-public class Analytics {
-
-	public static final String ATTRIBUTE_TIME = "time";
-	public static final String ATTRIBUTE_PASSCODE_USED = "passcode_used";
-}
